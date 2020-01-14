eddb63a04725e83653274d3aedd2293587d60a47
henricasi <henriquecmandruzato@gmail.com>
2020-01-14 12:41:42 -0300

    mongodb_uri fixed

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 // Mongoose Setup
 mongoose
- .connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+ .connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
  .catch(err => console.error('Error connecting to mongo', err));
 
